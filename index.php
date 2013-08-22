5fc93ca25a5a2d4d19c7af2d507a7841608baa6e
shock_one <shockone89@gmail.com>
2013-08-22 23:26:01 +0300

    Basic MVC layout

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 echo 'Hello, world!';
+$host = '127.0.0.1';
+$dbname = 'db';
+$port = 3307;
+$user = 'priscila';
+$password = 'wzpGsWHM';
+$DBH = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
 
+foreach ($DBH->query('SHOW TABLES') as $row) {
+	var_export($row);
+}
