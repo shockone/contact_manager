89b453ff4de1630f58c50436ab64093210e2b74c
shock_one <shockone89@gmail.com>
2013-08-26 22:27:24 +0300

    Trying to fix the issue with Config.php not found

---
+++ config/Config.php
@@ -0,0 +1,48 @@
+<?php
+
+class Config implements ArrayAccess {
+    private $collection = array();
+
+    function get($key){
+        if(isset($this->collection[$key])){
+            return $this->collection[$key];
+        }
+        return false;
+    }
+
+    function set($key, $value){
+        $this->collection[$key] = $value;
+    }
+
+    function offsetExists($offset) {
+        return isset($this->collection[$offset]);
+    }
+
+
+    function offsetGet($offset) {
+        return $this->get($offset);
+    }
+
+
+    function offsetSet($offset, $value) {
+        $this->set($offset, $value);
+    }
+
+
+    function offsetUnset($offset) {
+        unset($this->collection[$offset]);
+    }
+}
+
+$config = new Config();
+
+$config['DEVELOPMENT_ENVIRONMENT'] = 1;
+
+$config['DB_HOST']     = 'tunnel.pagodabox.com';
+$config['DB_NAME']     = 'db';
+$config['DB_PORT']     = 3306;
+$config['DB_USER']     = 'priscila';
+$config['DB_PASSWORD'] = 'wzpGsWHM';
+
+$config['DEFAULT_CONTROLLER'] = 'ContactsController';
+$config['DEFAULT_ACTION']     = 'index';
--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 define('ROOT_DIRECTORY', dirname(__DIR__));
-var_export(scandir(ROOT_DIRECTORY));
-var_export(scandir(ROOT_DIRECTORY . '/config/'));
-
 require_once(ROOT_DIRECTORY . '/config/Config.php');
 require_once(ROOT_DIRECTORY . '/library/startup.php');
 
