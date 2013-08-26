7fea68b2193c6315e99ea010208b35dfcfa1bff3
shock_one <shockone89@gmail.com>
2013-08-26 21:55:44 +0300

    Start working on the UI

--- application/views/layouts/application/header.php
+++ application/views/layouts/application/header.php
@@ -5,17 +5,130 @@
 
     <!-- jQuery -->
     <script src="//code.jquery.com/jquery-latest.min.js"></script>
+    <link rel="stylesheet" href="/css/main.css">
+    <script src="/js/list.min.js"></script>
+    <script src="/js/main.js"></script>
 
-    <!-- Bootstrap -->
-<!--    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">-->
-<!--    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">-->
-<!--    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>-->
 </head>
 <body>
-<nav>
-    <div>
-        <input id="search" type="search" placeholder="Search a contact">
-        <div id="add-contact">Add a contact</div>
+
+
+<div id="container">
+    <div id="contacts-list">
+        <div class="menu">
+            <span class="button add-button">Add new contact</span>
+            <input class="search" placeholder="Search contacts"/>
+            <span class="sort-by">
+                <span id="sort-by-text">Sort by:</span>
+                <span class="sort button" data-sort="name">First Name</span>
+                <span class="sort button" data-sort="category">Last Name</span>
+            </span>
+        </div>
+        <ul class="list">
+            <li>
+                <h4><span class="name">Monkey Island 2: LeChuck's Revenge</span> <span
+                        class="category">Game</span>
+                </h4>
+                <p class="description">Monkey Island 2: LeChuck's Revenge is an adventure game developed and
+                    published by LucasArts in 1991. It was the second game of the Monkey Island series,
+                    following The Secret...</p>
+            </li>
+            <li>
+                <h4><span class="name">Good Coffee</span> <span class="category">Beverage</span></h4>
+                <p class="description">Coffee is a brewed beverage with a dark, slightly acidic flavor
+                    prepared from the roasted seeds of the coffee plant, colloquially called coffee
+                    beans.</p>
+            </li>
+            <li>
+                <h4><span class="name">Full Throttle</span> <span class="category">Game</span></h4>
+                <p class="description">Full Throttle is a computer adventure game developed and published by
+                    LucasArts. It was designed by Tim Schafer, who would later go on to design the
+                    critically acclaimed titles Grim Fandango, Psychonauts and Brütal Legend.</p>
+            </li>
+            <li>
+                <h4><span class="name">Brooklyn Lager</span> <span class="category">Beverage</span></h4>
+
+                <p class="description">Brooklyn Brewery was started in 1987 by former Associated Press
+                    correspondent Steve Hindy and former Chemical Bank lending officer Tom Potter.</p>
+            </li>
+            <li>
+                <h4><span class="name">Monkey Island 2: LeChuck's Revenge</span> <span
+                        class="category">Game</span>
+                </h4>
+                <p class="description">Monkey Island 2: LeChuck's Revenge is an adventure game developed and
+                    published by LucasArts in 1991. It was the second game of the Monkey Island series,
+                    following The Secret...</p>
+            </li>
+            <li>
+                <h4><span class="name">Good Coffee</span> <span class="category">Beverage</span></h4>
+                <p class="description">Coffee is a brewed beverage with a dark, slightly acidic flavor
+                    prepared from the roasted seeds of the coffee plant, colloquially called coffee
+                    beans.</p>
+            </li>
+            <li>
+                <h4><span class="name">Full Throttle</span> <span class="category">Game</span></h4>
+                <p class="description">Full Throttle is a computer adventure game developed and published by
+                    LucasArts. It was designed by Tim Schafer, who would later go on to design the
+                    critically acclaimed titles Grim Fandango, Psychonauts and Brütal Legend.</p>
+            </li>
+            <li>
+                <h4><span class="name">Brooklyn Lager</span> <span class="category">Beverage</span></h4>
+
+                <p class="description">Brooklyn Brewery was started in 1987 by former Associated Press
+                    correspondent Steve Hindy and former Chemical Bank lending officer Tom Potter.</p>
+            </li>
+            <li>
+                <h4><span class="name">Monkey Island 2: LeChuck's Revenge</span> <span
+                        class="category">Game</span>
+                </h4>
+                <p class="description">Monkey Island 2: LeChuck's Revenge is an adventure game developed and
+                    published by LucasArts in 1991. It was the second game of the Monkey Island series,
+                    following The Secret...</p>
+            </li>
+            <li>
+                <h4><span class="name">Good Coffee</span> <span class="category">Beverage</span></h4>
+                <p class="description">Coffee is a brewed beverage with a dark, slightly acidic flavor
+                    prepared from the roasted seeds of the coffee plant, colloquially called coffee
+                    beans.</p>
+            </li>
+            <li>
+                <h4><span class="name">Full Throttle</span> <span class="category">Game</span></h4>
+                <p class="description">Full Throttle is a computer adventure game developed and published by
+                    LucasArts. It was designed by Tim Schafer, who would later go on to design the
+                    critically acclaimed titles Grim Fandango, Psychonauts and Brütal Legend.</p>
+            </li>
+            <li>
+                <h4><span class="name">Brooklyn Lager</span> <span class="category">Beverage</span></h4>
+
+                <p class="description">Brooklyn Brewery was started in 1987 by former Associated Press
+                    correspondent Steve Hindy and former Chemical Bank lending officer Tom Potter.</p>
+            </li>
+            <li>
+                <h4><span class="name">Monkey Island 2: LeChuck's Revenge</span> <span
+                        class="category">Game</span>
+                </h4>
+                <p class="description">Monkey Island 2: LeChuck's Revenge is an adventure game developed and
+                    published by LucasArts in 1991. It was the second game of the Monkey Island series,
+                    following The Secret...</p>
+            </li>
+            <li>
+                <h4><span class="name">Good Coffee</span> <span class="category">Beverage</span></h4>
+                <p class="description">Coffee is a brewed beverage with a dark, slightly acidic flavor
+                    prepared from the roasted seeds of the coffee plant, colloquially called coffee
+                    beans.</p>
+            </li>
+            <li>
+                <h4><span class="name">Full Throttle</span> <span class="category">Game</span></h4>
+                <p class="description">Full Throttle is a computer adventure game developed and published by
+                    LucasArts. It was designed by Tim Schafer, who would later go on to design the
+                    critically acclaimed titles Grim Fandango, Psychonauts and Brütal Legend.</p>
+            </li>
+            <li>
+                <h4><span class="name">Brooklyn Lager</span> <span class="category">Beverage</span></h4>
+
+                <p class="description">Brooklyn Brewery was started in 1987 by former Associated Press
+                    correspondent Steve Hindy and former Chemical Bank lending officer Tom Potter.</p>
+            </li>
+        </ul>
     </div>
-    <div></div>
-</nav>
+</div>
---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var options = {
+        valueNames: [ 'name', 'description', 'category' ]
+    };
+
+    new List('contacts-list', options);
+
+});
