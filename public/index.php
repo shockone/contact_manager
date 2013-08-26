2c0e3c7488a2ca078bc7be684316df93c5fd3101
shock_one <shockone89@gmail.com>
2013-08-26 22:13:12 +0300

    Trying to fix the issue with Config.php not found

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_DIRECTORY', dirname(__DIR__));
-require_once(ROOT_DIRECTORY . '/config/Config.php');
+require_once('../config/Config.php');
 require_once(ROOT_DIRECTORY . '/library/startup.php');
 
 dispatch();
