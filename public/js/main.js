49157bcd6a0232a8dbab119d3692ec41e2859eca
shock_one <shockone89@gmail.com>
2013-08-30 09:34:57 +0300

    Remove centering stuff

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,3 @@
-jQuery.fn.center = function () {
-    this.css("position","absolute");
-    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
-        $(window).scrollTop()) + "px");
-    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
-        $(window).scrollLeft()) + "px");
-    return this;
-}
-
 $(document).ready(function () {
     //List related stuff
     var options = {
@@ -93,7 +84,6 @@ $(document).ready(function () {
                 }else{
                     active.prev().click();
                 }
-                event.center();
             } else if (event.which == 40) { //down arrow
                 var active = $('.active');
                 if (!active.length) {
@@ -101,7 +91,6 @@ $(document).ready(function () {
                 }else{
                     active.next().click();
                 }
-                event.center();
             }
         }
     });
